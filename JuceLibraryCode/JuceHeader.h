60d7ba274f14c5eefbf9b22495e2066a4926b678
WuChang <3142324836@qq.com>
2024-06-12 21:10:31 +0800

    Update to JUCE 8.0.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
 
     IMPORTANT! This file is auto-generated each time you save your
     project - if you alter its contents, your changes may be overwritten!
@@ -12,7 +12,9 @@
 
 #pragma once
 
+
 #include <juce_analytics/juce_analytics.h>
+#include <juce_animation/juce_animation.h>
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
@@ -32,15 +34,21 @@
 #include <juce_product_unlocking/juce_product_unlocking.h>
 #include <juce_video/juce_video.h>
 
+
 #if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
- /** If you've hit this error then the version of the Projucer that was used to generate this project is
-     older than the version of the JUCE modules being included. To fix this error, re-save your project
-     using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
-     remove the JUCE_PROJUCER_VERSION define.
- */
- #error "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
+/** If you've hit this error then the version of the Projucer that was used to generate this project is
+    older than the version of the JUCE modules being included. To fix this error, re-save your project
+    using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
+    remove the JUCE_PROJUCER_VERSION define.
+*/
+#error "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
 #endif
 
+#if ! DONT_SET_USING_JUCE_NAMESPACE
+// If your code uses a lot of JUCE classes, then this will obviously save you
+// a lot of typing, but can be disabled by setting DONT_SET_USING_JUCE_NAMESPACE.
+using namespace juce;
+#endif
 
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
